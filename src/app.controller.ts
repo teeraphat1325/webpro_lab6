7f139d392f84457544f3302594b8e7883e653198
Teeraphat Prannok <66160068@go.buu.ac.th>
2025-01-27 18:33:45 +0700

    Controller & Http Methods & Path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  getDefault(): string {
+    return 'Default';
+  }
+
+  @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    return '<html><body><h1>Hello Buu</h1></body></html>';
+  }
+
+  @Delete('world')
+  getWorld(): string {
+    return '<html><body><h1>Hello Buu</h1></body></html>';
   }
 }
